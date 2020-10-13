3384e6c4f08b70f3426f969184c397650e0cd109
zhang <1311470691@qq.com>
2020-10-13 16:35:11 +0800

    miaoshu

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-list = ['xiaoming','xiaohei','xiaolv']
-str = 'starsdf'
-for item in str:
-    print(item)
-print(len(list))
+# aliens_0 = [
+#     {
+#         'color':'green',
+#         'points':10
+#     },
+#     {
+#         'color':'blue',
+#         'points':5
+#     }
+# ]
+# print(aliens_0)
+# exit()
+# del aliens_0['1']
+# for item in aliens_0[:1]:
+#     item['point'] = 20
+first_num = int(input('Please enter the number: '))
+last_num  = int(input('Please enter the number: '))
+print('结果为: '+str(first_num+last_num))
